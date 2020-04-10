23049171d2832bc63be0c554d193ce22cbf0c589
Freek Giele <freek.giele@gmail.com>
2020-04-10 13:37:43 +0200

    Included in reports listing

--- src/AzureDevOps.Report/BuildDurationReport.cs
+++ src/AzureDevOps.Report/BuildDurationReport.cs
@@ -10,17 +10,70 @@
 
 namespace AzureDevOps.Report
 {
+    using System;
     using AzureDevOps.Model;
 
+    /// <summary>
+    /// Build report definition.
+    /// </summary>
     public class BuildDurationReport : ReportDefinition, IReport
     {
-        public DataOptions DataOptions => throw new System.NotImplementedException();
+        /// <summary>
+        /// Gets data-options in use with the build report.
+        /// </summary>
+        public DataOptions DataOptions => DataOptions.Build;
 
-        public string Title => throw new System.NotImplementedException();
+        /// <summary>
+        /// Gets title of the build report.
+        /// </summary>
+        public string Title => $"BuildDurationReport-{DateTime.Now:yyyyMMdd-HHmmss}.csv";
 
+        /// <summary>
+        /// Parses the collected data and generates a CSV report.
+        /// </summary>
+        /// <param name="instance">Instance object containing the data collected from Azure DevOps.</param>
+        /// <returns>CSV string.</returns>
         public string Generate(AzureDevOpsInstance instance)
         {
-            throw new System.NotImplementedException();
+            if (instance == null)
+            {
+                throw new ArgumentNullException(nameof(instance));
+            }
+
+            this.CreateHeaders(
+                "Collection",
+                "Project",
+                "Repistory",
+                "Branch",
+                "Build nr",
+                "Build status",
+                "Build result",
+                "Build queued",
+                "Build start",
+                "Build end");
+
+            foreach (var collection in instance.Collections)
+            {
+                foreach (var project in collection.Projects)
+                {
+                    foreach (var build in project.Builds)
+                    {
+                            this.AddLine(
+                                    collection.Name,
+                                    project.Name,
+                                    build.Repository.Name,
+                                    build.SourceBranch,
+                                    build.BuildNumber,
+                                    build.Status,
+                                    build.Result,
+                                    build.QueueTime,
+                                    build.StartTime,
+                                    build.FinishTime);
+                    }
+                }
+            }
+
+            return this.GetReport();
         }
     }
 }
